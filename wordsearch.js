84493c27e9e0cb5c6217dc217a61adc3f41f46c7
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-16 21:38:08 +0000

    Function to find vertical and horizontal words with a 2D array (wordsearch). Pair programmed by Shradda Naidu & Iaan Johnston.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,45 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programmed by Shradda Naidu (@Shraddha-Naidu) and Iaan Johnston (@double-slide)
 
-module.exports = wordSearch
+// Function takes array of array of letters, and word to find (vertical or horizontal)
+// Function returns true if word is found vertically or horizontally
+// Function returns false if word is not found
+// Function does not look for words going backwards, upwards, or diagonal
+const wordSearch = (letters, word) => {
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let joinedLetters of horizontalJoin) {
+    if (joinedLetters.includes(word)) return true;
+  }
+
+  let transposeWordSearch = transpose(letters);
+
+  const fakeVerticalJoin = transposeWordSearch.map(ls => ls.join(''));
+  for (let joinedLetters of fakeVerticalJoin) {
+    if (joinedLetters.includes(word)) return true;
+  }
+
+  return false;
+
+};
+
+
+module.exports = wordSearch;
+
+
+// function takes input array
+// function returns new array that is transpose of input array
+const transpose = function(matrix) {
+
+  let rows = matrix.length;
+  let columns = matrix[0].length;
+  let transposeArray = [];
+
+  for (let ii = 0; ii < columns; ii++) {
+    transposeArray[ii] = [];
+
+    for (let jj = 0; jj < rows; jj++) {
+      transposeArray[ii].push(matrix[jj][ii]);
+    }
+  }
+  return transposeArray;
+};
